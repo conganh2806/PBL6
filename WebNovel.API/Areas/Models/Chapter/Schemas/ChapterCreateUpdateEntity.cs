c0e5da00c52ede7cf7c74b822920f7ede55e09bf
conganh2806 <ncanh2806@gmail.com>
2023-10-17 23:11:54 +0700

    feat: add chapter model, chapterdto and chapter controller

---
+++ WebNovel.API/Areas/Models/Chapter/Schemas/ChapterCreateUpdateEntity.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebNovel.API.Areas.Models.Chapter.Schemas
+{
+    public class ChapterCreateUpdateEntity
+    {
+        
+        public string Name {get; set;} = null!;
+        public bool IsLocked {get; set;}
+        public DateTime PublishDate {get; set;}
+        public int Views {get; set;}
+        public int Rating {get; set;}
+        public long FeeId {get; set;}
+        [MaxLength]
+        public string? FileContent {get; set;}
+        public int? Discount {get; set;}
+        public bool ApprovalStatus {get; set;}
+        public long NovelId {get; set;}
+    }
+}
