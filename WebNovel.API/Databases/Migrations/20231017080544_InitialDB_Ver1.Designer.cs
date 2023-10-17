e605e285a3c342fbde2bff7e269a940a6d3615f8
Vo Huu Nam Truong <namtruong0900@gmail.com>
2023-10-17 15:10:18 +0700

    feat: add new migrations

--- WebNovel.API/Databases/Migrations/20231017080544_InitialDB_Ver1.Designer.cs
+++ WebNovel.API/Databases/Migrations/20231017080544_InitialDB_Ver1.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Webnovel.API.Databases;
 
@@ -10,9 +11,11 @@ using Webnovel.API.Databases;
 namespace Webnovel.API.Databases.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231017080544_InitialDB_Ver1")]
+    partial class InitialDB_Ver1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
