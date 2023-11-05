0d8686787911d080b544ea0e73f9211a88a06edd
Vo Huu Nam Truong <namtruong0900@gmail.com>
2023-11-05 11:46:42 +0700

    fix [Database]: modelBuilder

--- WebNovel.API/Databases/ModelCreate.cs
+++ WebNovel.API/Databases/ModelCreate.cs
@@ -85,6 +85,14 @@ namespace WebNovel.API.Databases
             .HasForeignKey(e => e.AccountId)
             .IsRequired();
 
+            modelBuilder
+            .Entity<Comment>()
+            .HasKey(e => new
+            {
+                e.NovelId,
+                e.AccountId
+            });
+
             modelBuilder.Entity<Chapter>()
             .HasOne(e => e.Novel)
             .WithMany(e => e.Chapters)
