d2ad0dc287af068beb095cf5a3146707dcbdec56
Vo Huu Nam Truong <namtruong0900@gmail.com>
2023-11-05 23:26:52 +0700

    fix [Auth]: change authorize of controllers

--- WebNovel.API/Areas/Controllers/AccountController.cs
+++ WebNovel.API/Areas/Controllers/AccountController.cs
@@ -56,6 +56,7 @@ namespace WebNovel.API.Areas.Controllers
 
         [HttpPost]
         [ProducesResponseType(typeof(ResponseInfo), (int)HttpStatusCode.OK)]
+        [Authorize]
         public async Task<IActionResult> Create([FromBody] AccountCreateUpdateEntity account)
         {
             try
