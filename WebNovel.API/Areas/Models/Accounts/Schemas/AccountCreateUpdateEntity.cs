ab3ec12646595ed021d55cbc246753d467c980b3
Vo Huu Nam Truong <namtruong0900@gmail.com>
2023-11-11 20:35:14 +0700

    fix [Account]: Remove Unnecessary

--- WebNovel.API/Areas/Models/Accounts/Schemas/AccountCreateUpdateEntity.cs
+++ WebNovel.API/Areas/Models/Accounts/Schemas/AccountCreateUpdateEntity.cs
@@ -6,6 +6,42 @@ using System.Threading.Tasks;
 
 namespace WebNovel.API.Areas.Models.Accounts.Schemas
 {
+    public class AccountCreateEntity
+    {
+        [StringLength(100, ErrorMessage = "E005")]
+        public string NickName { get; set; } = null!;
+        [StringLength(100, ErrorMessage = "E005")]
+        public string Username { get; set; } = null!;
+        [StringLength(255, ErrorMessage = "E005")]
+        public string Password { get; set; } = null!;
+        [StringLength(255, ErrorMessage = "E005")]
+        public string ConfirmPassword { get; set; } = null!;
+        [StringLength(255, ErrorMessage = "E005")]
+        public string Email { get; set; } = null!;
+        public bool IsAdmin { get; set; }
+        public List<string> RoleIds { set; get; } = null!;
+    }
+
+    public class AccountUpdateEntity
+    {
+        public string Id { get; set; } = null!;
+        [StringLength(100, ErrorMessage = "E005")]
+        public string? NickName { get; set; } = null!;
+        [StringLength(100, ErrorMessage = "E005")]
+        public string? Username { get; set; } = null!;
+        [StringLength(255, ErrorMessage = "E005")]
+        public string? Password { get; set; } = null!;
+        [StringLength(255, ErrorMessage = "E005")]
+        public string? ConfirmPassword { get; set; } = null!;
+        [StringLength(255, ErrorMessage = "E005")]
+        public string? Email { get; set; } = null!;
+        public bool? IsAdmin { get; set; }
+        public List<string> RoleIds { set; get; } = null!;
+        public string? Phone { get; set; }
+        public float? WalletAmmount { get; set; }
+        public bool? IsActive { get; internal set; }
+    }
+
     public class AccountCreateUpdateEntity
     {
         public string? Id { get; set; }
@@ -20,7 +56,7 @@ namespace WebNovel.API.Areas.Models.Accounts.Schemas
         public string ConfirmPassword { get; set; } = null!;
         public string? Phone { get; set; }
         public float WalletAmmount { get; set; } = 0.0f;
-        public List<string> RoleIds { set; get; }
+        public List<string> RoleIds { set; get; } = null!;
         public bool IsAdmin { get; set; }
         public bool IsActive { get; set; }
         public string? RefreshToken { get; set; }
--- WebNovel.API/Areas/Models/Accounts/Schemas/AccountDto.cs
+++ WebNovel.API/Areas/Models/Accounts/Schemas/AccountDto.cs
@@ -9,18 +9,15 @@ namespace WebNovel.API.Areas.Models.Accounts.Schemas
 {
     public class AccountDto
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = null!;
         [StringLength(100)]
         public string Username { get; set; } = null!;
         [StringLength(255)]
-        public string Password { get; set; } = null!;
-        [StringLength(255)]
         public string Email { get; set; } = null!;
-        public List<string> RoleName { get; set; } = null!;
         public string NickName { get; set; } = null!;
         public int Status { get; set; }
         public string? Phone { get; set; }
-        public List<string> RoleIds { set; get; }
+        public List<string> RoleIds { set; get; } = null!;
         public bool IsAdmin { get; set; }
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpiryTime { get; set; }
