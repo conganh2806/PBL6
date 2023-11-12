8a520b422c168d721615d9c495354c46439805d1
Vo Huu Nam Truong <namtruong0900@gmail.com>
2023-11-12 13:41:22 +0700

    fix [Bookmarked]: Intergrade NovelDto into BookmarkedDto

--- WebNovel.API/Areas/Models/Bookmarkeds/Schemas/BookmarkedDto.cs
+++ WebNovel.API/Areas/Models/Bookmarkeds/Schemas/BookmarkedDto.cs
@@ -1,5 +1,3 @@
-using WebNovel.API.Areas.Models.Novels.Schemas;
-
 namespace WebNovel.API.Areas.Models.Bookmarked.Schemas
 {
     public class BookmarkedDto
@@ -7,6 +5,18 @@ namespace WebNovel.API.Areas.Models.Bookmarked.Schemas
         public string AccountId { get; set; } = null!;
         public string NovelId { get; set; } = null!;
         public string ChapterId { get; set; } = null!;
-        public NovelDto Novel { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public int Year { get; set; }
+        public int Views { get; set; }
+        public int Rating { get; set; }
+        public string ImagesURL { get; set; } = null!;
+        public List<string> GenreName { get; set; } = null!;
+        public List<long> GenreIds { get; set; } = null!;
+        public string? Description { get; set; }
+        public bool Status { get; set; }
+        public bool ApprovalStatus { get; set; }
+        public int NumChapter { get; set; }
     }
 }
